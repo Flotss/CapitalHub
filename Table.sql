2964323c27afca4b5940869c7845575e75a0d75b
Flotss <manginf54@gmail.com>
2024-01-10 00:27:06 +0100

    feat(sql): rename column and refactor some tables

--- Table.sql
+++ Table.sql
@@ -3,88 +3,99 @@ drop table if exists History;
 drop table if exists Transaction;
 drop table if exists TransactionType;
 drop table if exists Currency;
-drop table if exists PortefeuilleProduit;
-drop table if exists Actions;
+drop table if exists ActionProduit;
+drop table if exists Action;
 drop table if exists Portefeuille;
 
-create table if not exists Portefeuille
+CREATE SEQUENCE if not exists portefeuille_id_sequence START 1;
+CREATE SEQUENCE if not exists actions_id_sequence START 1;
+CREATE SEQUENCE if not exists actionproduit_id_sequence START 1;
+CREATE SEQUENCE if not exists transaction_id_sequence START 1;
+CREATE SEQUENCE if not exists history_id_sequence START 1;
+CREATE SEQUENCE if not exists event_id_sequence START 1;
+
+ALTER SEQUENCE portefeuille_id_sequence RESTART WITH 1;
+ALTER SEQUENCE actions_id_sequence RESTART WITH 1;
+ALTER SEQUENCE actionproduit_id_sequence RESTART WITH 1;
+ALTER SEQUENCE transaction_id_sequence RESTART WITH 1;
+ALTER SEQUENCE history_id_sequence RESTART WITH 1;
+ALTER SEQUENCE event_id_sequence RESTART WITH 1;
+
+CREATE TABLE IF NOT EXISTS Portefeuille
 (
-    idPortefeuille          int          not null,
-    idUtilisateur           int not null,
-    nomPortefeuille         varchar(255) not null,
-    descriptionPortefeuille varchar(255),
-    primary key (idPortefeuille)
+    id          INT DEFAULT nextval('portefeuille_id_sequence') NOT NULL,
+    idUser      INT                                             NOT NULL,
+    name        VARCHAR(255)                                    NOT NULL,
+    description VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
-create table if not exists Actions
+CREATE TABLE IF NOT EXISTS Action
 (
-    idmarche int   not null,
-    nom      varchar(225) not null,
-    valeur   float not null,
-    primary key (idmarche)
+    id    INT DEFAULT nextval('actions_id_sequence') NOT NULL,
+    name  VARCHAR(225)                               NOT NULL,
+    value FLOAT                                      NOT NULL,
+    PRIMARY KEY (id)
 );
 
-create table if not exists PortefeuilleProduit
+CREATE TABLE IF NOT EXISTS ActionProduit
 (
-    idProduit int not null,
-    quantité       int          not null,
-    idPortefeuille int          not null,
-    primary key (idProduit, idPortefeuille),
-    foreign key (idPortefeuille) references Portefeuille (idPortefeuille),
-    foreign key (idProduit) references Actions (idMarche)
+    id             INT DEFAULT nextval('actionproduit_id_sequence') NOT NULL,
+    idPortefeuille INT                                              NOT NULL,
+    quantity       INT                                              NOT NULL,
+    PRIMARY KEY (id, idPortefeuille),
+    FOREIGN KEY (idPortefeuille) REFERENCES Portefeuille (id),
+    FOREIGN KEY (id) REFERENCES Action (id)
 );
 
-
-create table if not exists Currency
+CREATE TABLE IF NOT EXISTS Currency
 (
-    code    varchar(255) not null,
-    name    varchar(255) not null,
-    symbole varchar(255) not null,
-    primary key (code)
+    code    VARCHAR(255) NOT NULL,
+    name    VARCHAR(255) NOT NULL,
+    symbole VARCHAR(255) NOT NULL,
+    PRIMARY KEY (code)
 );
 
-create table if not exists TransactionType
+CREATE TABLE IF NOT EXISTS TransactionType
 (
-    transactionType varchar(255) not null,
-    primary key (transactionType)
+    transactionType VARCHAR(255) NOT NULL,
+    PRIMARY KEY (transactionType)
 );
 
-
-create table if not exists Transaction
+CREATE TABLE IF NOT EXISTS Transaction
 (
-    idTransaction   int          not null,
-    idPortefeuille  int          not null,
-    prix            float        not null,
-    dateTransaction date         not null,
-    codeCurrency varchar(255) not null,
-    transactionType varchar(255) not null,
-    valeur       float        not null,
-    primary key (idTransaction),
-    foreign key (idPortefeuille) references Portefeuille (idPortefeuille),
-    foreign key (codeCurrency) references Currency (code),
-    foreign key (transactionType) references TransactionType (transactionType)
+    id             INT DEFAULT nextval('transaction_id_sequence') NOT NULL,
+    idPortefeuille INT                                            NOT NULL,
+    idAction       INT                                            NOT NULL,
+    price          FLOAT                                          NOT NULL,
+    date           timestamp                                      NOT NULL,
+    codeCurrency   VARCHAR(255)                                   NOT NULL,
+    type           VARCHAR(255)                                   NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idPortefeuille) REFERENCES Portefeuille (id),
+    FOREIGN KEY (idAction) REFERENCES Action (id),
+    FOREIGN KEY (codeCurrency) REFERENCES Currency (code),
+    FOREIGN KEY (type) REFERENCES TransactionType (transactionType)
 );
 
-
-
-create table if not exists History
+CREATE TABLE IF NOT EXISTS History
 (
-    idHistory      int not null,
-    idPortefeuille int not null,
-    primary key (idHistory),
-    foreign key (idPortefeuille) references Portefeuille (idPortefeuille)
+    id             INT DEFAULT nextval('history_id_sequence') NOT NULL,
+    idPortefeuille INT                                        NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idPortefeuille) REFERENCES Portefeuille (id)
 );
 
-create table if not exists Event
+CREATE TABLE IF NOT EXISTS Event
 (
-    idEvent       int          not null,
-    idHistory     int          not null,
-    idTransaction int          not null,
-    dateEvent     date         not null,
-    description varchar(255),
-    primary key (idEvent),
-    foreign key (idHistory) references History (idHistory),
-    foreign key (idTransaction) references Transaction (idTransaction)
+    id            INT DEFAULT nextval('event_id_sequence') NOT NULL,
+    idHistory     INT                                      NOT NULL,
+    idTransaction INT                                      NOT NULL,
+    dateEvent     timestamp                                NOT NULL,
+    description   VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (idHistory) REFERENCES History (id),
+    FOREIGN KEY (idTransaction) REFERENCES Transaction (id)
 );
 
 insert into Currency (code, name, symbole)
@@ -99,49 +110,91 @@ insert into TransactionType (transactionType)
 values ('Sell');
 
 INSERT INTO Portefeuille
-(idportefeuille, idutilisateur, nomportefeuille, descriptionportefeuille)
-VALUES (1, 1, 'TestPorteFeuille', 'Mon Test de portefeuille');
-INSERT INTO Portefeuille (idportefeuille, idutilisateur, nomportefeuille, descriptionportefeuille)
-VALUES (2, 1, 'TestPorteFeuille2', 'Mon Test de portefeuille2');
+    (iduser, name, description)
+VALUES (1, 'TestPorteFeuille', 'Mon Test de portefeuille');
+INSERT INTO Portefeuille (iduser, name, description)
+VALUES (1, 'TestPorteFeuille2', 'Mon Test de portefeuille2');
 
-insert into Actions (idmarche, nom, valeur)
-values (1, 'Bitcoin', 10000);
-insert into Actions (idmarche, nom, valeur)
-values (2, 'Ethereum', 1000);
-insert into Actions (idmarche, nom, valeur)
-values (3, 'Tesla', 233.96);
-insert into Actions (idmarche, nom, valeur)
-values (4, 'Apple', 133.11);
-insert into Actions (idmarche, nom, valeur)
-values (5, 'Amazon', 3206.22);
-insert into Actions (idmarche, nom, valeur)
-values (6, 'Facebook', 257.62);
-insert into Actions (idmarche, nom, valeur)
-values (7, 'Netflix', 503.18);
+insert into Action (name, value)
+values ('Bitcoin', 10000);
+insert into Action (name, value)
+values ('Ethereum', 1000);
+insert into Action (name, value)
+values ('Tesla', 233.96);
+insert into Action (name, value)
+values ('Apple', 133.11);
+insert into Action (name, value)
+values ('Amazon', 3206.22);
+insert into Action (name, value)
+values ('Facebook', 257.62);
+insert into Action (name, value)
+values ('Netflix', 503.18);
 
 
+-- Transactions pour Bitcoin (Action ID 1)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 1, 10000, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (2, 1, 1000, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 1, 3000, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 1, 50, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (2, 1, 500, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
 
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (1, 1, 10, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (2, 1, 10, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (3, 1, 10, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (4, 1, 10, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (5, 1, 10, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell', 10);
+-- Transactions pour Ethereum (Action ID 2)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 2, 1000, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 2, 1100, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 2, 500, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 2, 100, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 2, 480, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
 
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (6, 2, 10, timestamp '2024-01-01 14:00:00', 'EUR', 'Sell', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (7, 2, 10, timestamp '2024-01-01 15:00:00', 'EUR', 'Buy', 100);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (8, 2, 10, timestamp '2024-01-01 16:00:00', 'EUR', 'Sell', 10);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (9, 2, 10, timestamp '2024-01-01 17:00:00', 'EUR', 'Buy', 200);
-insert into Transaction (idTransaction, idPortefeuille, prix, dateTransaction, codeCurrency, transactionType, valeur)
-values (10, 2, 10, timestamp '2024-01-01 18:00:00', 'EUR', 'Sell', 100);
+-- Transactions pour Tesla (Action ID 3)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 3, 500, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 3, 100, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 3, 100, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 3, 127, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 3, 50, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
+
+-- Transactions pour Apple (Action ID 4)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 4, 300, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 4, 150, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 4, 300, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 4, 200, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 4, 50, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
+
+-- Transactions pour Amazon (Action ID 5)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 5, 3000, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 5, 2000, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 5, 4000, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 5, 3000, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 5, 1000, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
+
+-- Transactions pour Facebook (Action ID 6)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 6, 200, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 6, 250, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 6, 50, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 6, 40, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 6, 200, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
+
+-- Transactions pour Netflix (Action ID 7)
+INSERT INTO Transaction (idPortefeuille, idAction, price, date, codeCurrency, type)
+VALUES (1, 7, 503.18, timestamp '2024-01-01 9:00:00', 'EUR', 'Buy'),
+       (1, 7, 505.00, timestamp '2024-01-01 10:00:00', 'EUR', 'Buy'),
+       (1, 7, 499.75, timestamp '2024-01-01 11:00:00', 'EUR', 'Sell'),
+       (1, 7, 510.50, timestamp '2024-01-01 12:00:00', 'EUR', 'Buy'),
+       (1, 7, 300, timestamp '2024-01-01 13:00:00', 'EUR', 'Sell');
+
+
+-- -- select all action from portefeuille -> transaction -> action
+-- select Distinct (a.*)
+-- from Portefeuille p
+--          join Transaction t on p.id = t.idPortefeuille
+--          join Action a on t.idAction = a.id
+-- where p.id = 1;
+
 
 
 -- delete from Transaction where idTransaction = 1;
