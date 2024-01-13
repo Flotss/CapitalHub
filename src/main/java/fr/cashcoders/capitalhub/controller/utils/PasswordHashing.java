4c392e9d22c4d7b69d9f9b994026b722c379b9ec
Flotss <manginf54@gmail.com>
2024-01-13 17:28:26 +0100

    feat(User): Add password hashing, user retrieval and insertion methods, and database interface methods
    This commit adds the `PasswordHashing` class for hashing passwords, as well as methods for retrieving and inserting users from the database. It also implements the `DBInterface` for the User class, providing methods for saving, updating, and deleting user records in the database.

---
+++ src/main/java/fr/cashcoders/capitalhub/controller/utils/PasswordHashing.java
@@ -0,0 +1,37 @@
+package fr.cashcoders.capitalhub.controller.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHashing {
+    public static String hashPassword(String password) {
+        try {
+            // Créez un objet MessageDigest avec l'algorithme SHA-256
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Convertissez le mot de passe en tableau de bytes
+            byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+
+            // Calculez le hachage en utilisant les bytes du mot de passe
+            byte[] hashedBytes = digest.digest(passwordBytes);
+
+            // Convertissez le hachage en représentation hexadécimale
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : hashedBytes) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+
+            // Retournez le hachage sous forme de chaîne hexadécimale
+            return stringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Gérez l'exception NoSuchAlgorithmException
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hashPassword("test2"));
+    }
+}
--- src/main/java/fr/cashcoders/capitalhub/model/User.java
+++ src/main/java/fr/cashcoders/capitalhub/model/User.java
@@ -1,7 +1,13 @@
 package fr.cashcoders.capitalhub.model;
 
-public class User {
-    private final int id;
+import fr.cashcoders.capitalhub.controller.utils.PasswordHashing;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User implements DBInterface {
+    private int id;
     private final String username;
 
 
@@ -18,5 +24,60 @@ public class User {
         return username;
     }
 
+    public static User getUserFromDB(String username, String password) throws SQLException {
+        String hashPassword = PasswordHashing.hashPassword(password);
 
+
+        PreparedStatement a = connection.prepareStatement("SELECT * FROM UserTable WHERE username = ? AND password = ?");
+        a.setString(1, username);
+        a.setString(2, hashPassword);
+        a.executeQuery();
+        ResultSet resultSet = a.getResultSet();
+        if (resultSet.next()) {
+            System.out.println("User found");
+            System.out.println(resultSet.getInt("id"));
+            System.out.println(resultSet.getString("username"));
+            return new User(resultSet.getInt("id"), resultSet.getString("username"));
+        }
+        return null;
+    }
+
+    public static User insertNewUser(String username, String password) throws SQLException {
+        String hashPassword = PasswordHashing.hashPassword(password);
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO UserTable (username, password) VALUES (?, ?) RETURNING id, username;");
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, hashPassword);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            return new User(resultSet.getInt("id"), resultSet.getString("username"));
+        }
+        return null;
+    }
+
+
+    @Override
+    public void save() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO UserTable (username, password) VALUES (?, ?) RETURNING id;");
+        preparedStatement.setString(1, this.username);
+        preparedStatement.setString(2, PasswordHashing.hashPassword("password"));
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            this.id = resultSet.getInt("id");
+        }
+    }
+
+    @Override
+    public void update() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE UserTable SET username = ? WHERE id = ?;");
+        preparedStatement.setString(1, this.username);
+        preparedStatement.setInt(2, this.id);
+        preparedStatement.executeUpdate();
+    }
+
+    @Override
+    public void delete() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM UserTable WHERE id = ?;");
+        preparedStatement.setInt(1, this.id);
+        preparedStatement.executeUpdate();
+    }
 }
